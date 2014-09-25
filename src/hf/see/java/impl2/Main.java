dd2e775f3d93ba39a91d048cb09ca73249b6a228
Elia Seikritt <elia.seikritt@hotmail.com>
2014-09-25 13:57:54 +0200

    Verwenden von MeasurableString

--- src/hf/see/java/impl2/Main.java
+++ src/hf/see/java/impl2/Main.java
@@ -25,20 +25,44 @@ public class Main {
 		for(Iterator<IMeasurable> i = TestData.iterator();i.hasNext();)
 		{
 			IMeasurable currentQuizResult = i.next();
-			System.out.println(currentQuizResult.getName() + " mit " + currentQuizResult.getPoints() + " Punkten");
+			System.out.println(currentQuizResult.getName() + " mit " + currentQuizResult.getScore() + " Punkten");
 		}
 		System.out.println();
 		
-		IMeasurable winner = 		DataSet.calcMax(PreDefinedValueAccessors.byPoints());
+		IMeasurable winner = 		DataSet.calcMax(PreDefinedValueAccessors.byScore());
 		IMeasurable longestName = 	DataSet.calcMax(PreDefinedValueAccessors.byNameLen());
-		double avgPoints = 			DataSet.calcAvg(PreDefinedValueAccessors.byPoints());
+		double avgPoints = 			DataSet.calcAvg(PreDefinedValueAccessors.byScore());
 		double avgNameLen = 		DataSet.calcAvg(PreDefinedValueAccessors.byNameLen());
 		
-		System.out.printf( "Der Gewinner heisst %s und hat %d Punkt(e)%n", winner.getName(), winner.getPoints() );
+		System.out.printf( "Der Gewinner heisst %s und hat %d Punkt(e)%n", winner.getName(), winner.getScore() );
 		System.out.printf( "Der längste Name ist %s und hat %d Buchstaben%n", longestName.getName(), longestName.getName().length() );
 		System.out.println("Der Durchschnitt der Punktzahl ist " + avgPoints);
 		System.out.println("Die durchschnittliche Namenslänge ist " + avgNameLen + " Buchstaben");
-
+		System.out.println();
+		
+		TestData.clear();
+		TestData.add(new MeasurableString("dfsdfsdfd"));
+		TestData.add(new MeasurableString("oigg"));
+		TestData.add(new MeasurableString("dwee"));
+		TestData.add(new MeasurableString("attrdc"));
+		TestData.add(new MeasurableString("xcvxcv"));
+		TestData.add(new MeasurableString("rttzzertrr"));
+		DataSet = new DataSet(TestData);
+		
+		System.out.println("Dies sind die Strings:");
+		for(Iterator<IMeasurable> i = TestData.iterator();i.hasNext();)
+		{
+			IMeasurable currentQuizResult = i.next();
+			System.out.println(currentQuizResult.getName() + " mit " + currentQuizResult.getScore() + " Buchstaben");
+		}
+		System.out.println();
+		
+		longestName = 	DataSet.calcMax(PreDefinedValueAccessors.byScore());
+		avgNameLen = 	DataSet.calcAvg(PreDefinedValueAccessors.byScore());
+		
+		System.out.printf( "Der längste String ist '%s' und hat %d Buchstaben%n", longestName.getName(), longestName.getName().length() );
+		System.out.printf("Die durchschnittliche Stringlänge ist %.3f Buchstaben%n", avgNameLen);
+		System.out.println();
 	}
 
 }
